69a538a39ead8c48a089dc6e0c62d48086e16fc0
Caue Costacurta <cauecostacurta@outlook.com>
2025-01-23 16:29:40 -0300

    Pequenas alterações

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("ola, mundo")
+# print("ola, mundo")
+# print(3)
+# print("Olá" + " " + "turma!")
+
+# print(len(input("Digite seu nome: ")))
+
+num1 = int((input("Digite um numero: ")))
+num2 = int((input("Digite outro numero: ")))
+total = num1 + num2
+
+print(f"A soma dos numeros eh: {total}")
