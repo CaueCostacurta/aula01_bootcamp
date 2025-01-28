4de67b1fb5e99a16be62d096418cf50b9732a800
Caue Costacurta <cauecostacurta@outlook.com>
2025-01-28 13:54:20 -0300

    correção do input do usuario

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 
 # print(len(input("Digite seu nome: ")))
 
-num1 = int((input("Digite um numero: ")))
-num2 = int((input("Digite outro numero: ")))
-total = num1 + num2
+# num1 = int((input("Digite um numero: ")))
+# num2 = int((input("Digite outro numero: ")))
+# total = num1 + num2
 
 #print(f"A soma dos numeros eh: {total}")
 
-print(input("Digite seu nome") + input("Digite seu sobrenome"))
+print(input("Digite seu nome: ") + " " + input("Digite seu sobrenome: "))
